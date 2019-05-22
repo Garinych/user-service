d086a17f3c67835063bdd0b036fc4193698b26c9
dumitru.sandul <dumitru.sandul@codefactorygroup.com>
2019-05-22 11:16:12 +0300

    compile changes

--- build/generated/sources/annotationProcessor/java/main/md/codefactory/userservice/mapping/UserMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/md/codefactory/userservice/mapping/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2019-05-22T09:32:25+0300",
+    date = "2019-05-22T10:41:34+0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.1 (Oracle Corporation)"
 )
 @Component
