9e4784da0bc29e870e1efcaf91729444886710b3
dumitru.sandul <dumitru.sandul@codefactorygroup.com>
2019-05-27 14:51:36 +0300

    Created saveUser , updateProfile, viewProfile test for UserController

--- src/test/java/md/codefactory/userservice/controller/UsersControllerIntegrationTest.java
+++ src/test/java/md/codefactory/userservice/controller/UsersControllerIntegrationTest.java
@@ -1,10 +1,15 @@
 package md.codefactory.userservice.controller;
 
+import md.codefactory.userservice.domain.User;
 import md.codefactory.userservice.mapping.UserMapper;
 import md.codefactory.userservice.mapping.dto.NewUserDto;
+import md.codefactory.userservice.mapping.dto.UpdateUserDto;
+import md.codefactory.userservice.repository.UsersRepository;
+import md.codefactory.userservice.util.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -14,7 +19,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -28,10 +35,11 @@ public class UsersControllerIntegrationTest {
     private WebApplicationContext webApplicationContext;
 
     @Autowired
+    private UsersRepository usersRepository;
+
     private MockMvc mockMvc;
 
-    @Autowired
-    private UserMapper userMapper;
+    private final UserMapper userMapper = Mappers.getMapper(UserMapper.class);
 
     @Before
     public void setUp() throws Exception {
@@ -42,34 +50,61 @@ public class UsersControllerIntegrationTest {
     }
 
     @Test
-    public void saveUser() throws Exception{
-        NewUserDto newUserDto = new NewUserDto();
-        newUserDto.setFirstName("Vasea");
-        newUserDto.setLastName("Gliuck");
-        newUserDto.setEmail("vasea.g@gmail.com");
-        newUserDto.setPhoneNumber("069332211");
-        newUserDto.setUsername("vasea.gliuck");
-        newUserDto.setPassword("vasea123");
+    public void should_save_new_user() throws Exception{
+        User newUser = new User();
+        newUser.setFirstName("Vasea");
+        newUser.setLastName("Gliuck");
+        newUser.setEmail("vasea.g@gmail.com");
+        newUser.setPhoneNumber("069332211");
+        newUser.setUsername("vasea.gliuck");
+        newUser.setPassword("vasea123");
 
-        this.mockMvc.perform(post("/api/user-services/users/register")
+        NewUserDto newUserDto = userMapper.userToNewUserDto(newUser);
+
+        this.mockMvc.perform(post("/api/users")
                 .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtils.objectAsBytes(newUserDto)))
                 .andExpect(status().isCreated());
     }
 
     @Test
-    public void userProfile() {
+    public void should_show_user_profile() throws Exception{
+
+        this.mockMvc.perform(get("/api/users/{id}", 1)
+            .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        this.mockMvc.perform(get("/api/users/{id}", -1)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("errorMessage", is("User with id = -1 not found!")));
+
     }
 
     @Test
-    public void updateUser() {
+    public void should_update_user_profile() throws Exception {
+
+        UpdateUserDto updatedUser = new UpdateUserDto();
+        updatedUser.setFirstName("Vasea");
+        updatedUser.setLastName("Gliuc");
+        updatedUser.setEmail("vsea@codefactory.com");
+        updatedUser.setPhoneNumber("069545452");
+        updatedUser.setUsername("vsea2");
+        updatedUser.setPassword("jora123");
+
+        this.mockMvc.perform(put("/api/users/{id}", 2)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtils.objectAsBytes(updatedUser)))
+                .andExpect(status().isOk());
+
+        this.mockMvc.perform(put("/api/users/{id}", 3)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtils.objectAsBytes(updatedUser)))
+                .andExpect(status().isConflict());
+
     }
 
-    @Test
-    public void deleteUser() {
-    }
-
-    @Test
-    public void findAllUsers() {
-    }
 }
