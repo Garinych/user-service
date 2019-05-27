5c4da16f7973d36383fad103660010f36f611707
dumitru.sandul <dumitru.sandul@codefactorygroup.com>
2019-05-27 12:23:47 +0300

    created tests for User and Role repositories

---
+++ src/test/java/md/codefactory/userservice/repository/RoleRepositoryTest.java
@@ -0,0 +1,33 @@
+package md.codefactory.userservice.repository;
+
+import md.codefactory.userservice.domain.Role;
+import md.codefactory.userservice.domain.enums.RoleName;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(value = {"/sql-scripts/data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+public class RoleRepositoryTest {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Test
+    public void find_by_role_name() {
+        Optional<Role> actualRole = roleRepository.findByName(RoleName.ROLE_USER);
+        assertTrue(actualRole.isPresent());
+    }
+
+}
